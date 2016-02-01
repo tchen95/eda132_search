63a1840ac945f19b8938ae18f7dbb15b553c3d35
emily2zhou <emily2zhou@gmail.com>
2016-02-01 22:37:25 +0100

    testing added for player.java

--- othello.java
+++ othello.java
@@ -4,6 +4,9 @@ public class othello {
 
     public static void main(String[] args) {
         saySomething("Hello World");
+	player p1 = new player("Lester");
+	p1.addTally(3);
+	System.out.println(p1.getName()+"......tally = "+p1.getTally());
     }
 
     public static void saySomething(String string) {
