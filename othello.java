2d31ab7c1708204f37819d099280589e3d24dc75
Tammy Chen <tchen95@berkeley.edu>
2016-02-01 21:04:32 +0100

    Commiting othello class and java files

---
+++ othello.java
@@ -0,0 +1,11 @@
+public class othello {
+
+    public static void main(String[] args) {
+        saySomething("Hello World");
+    }
+
+    public static void saySomething(String string) {
+    	System.out.println(string);
+    }
+
+}
