a836292371fd1ed94a5ef058f87f9226b072879d
Tammy Chen <tchen95@berkeley.edu>
2016-02-01 23:05:59 +0100

    changes to gameboard

--- gameboard.java
+++ gameboard.java
@@ -11,17 +11,18 @@ public class gameboard {
 
 	public void populateBoard() {
 		for (int index = 0; index < 8; index++) {
-			ArrayList<String> newRow = new ArrayList<String>(Collections.nCopies(8, "/"));
+			ArrayList<String> newRow = new ArrayList<String>(Collections.nCopies(8, " /"));
 			board.add(index, newRow);
 		}
-		board.get(3).set(3, "O");
-		board.get(3).set(4, "X");
-		board.get(4).set(3, "X");
-		board.get(4).set(4, "O");
+		board.get(3).set(3, " O");
+		board.get(3).set(4, " X");
+		board.get(4).set(3, " X");
+		board.get(4).set(4, " O");
 	}
 
 	public void printBoard() {
 		for (int index = 0; index < 8; index++) {
+			System.out.print(index+1);
 			for (int innerIndex = 0; innerIndex < 8; innerIndex++) {
 				if (innerIndex == 7) {
 					System.out.println(board.get(index).get(innerIndex));
@@ -30,5 +31,6 @@ public class gameboard {
 				}
 			}
 		}
+		System.out.println("  a b c d e f g h");
 	}
 }
