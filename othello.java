2f7c30aec64b8494239577059476c61bd6883a5d
emily2zhou <emily2zhou@gmail.com>
2016-02-02 21:33:03 +0100

    added some print statements for the scores

--- othello.java
+++ othello.java
@@ -18,6 +18,9 @@ public class othello {
 		while (!gameComplete) {
 			if (doesPlayerHaveMoves(currentPlayer, currentGameboard)) {
 			    currentGameboard.printBoard();
+			    System.out.println(currentPlayer.getName() + ": " + currentPlayer.getTally() +
+					       "," + otherPlayer.getName() + ": " + otherPlayer.getTally());
+			    
 			    Scanner reader = new Scanner(System.in);
 			    System.out.println("What move would you like to make?");
 			    String input = reader.next();	    
